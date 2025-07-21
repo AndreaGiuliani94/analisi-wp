7984da689dd8679975bf1b507ebdc6daf930e6e8
AndreGiuliani94 <57347381+AndreGiuliani94@users.noreply.github.com>
2025-07-21 16:43:51 +0200

    minor fix

--- src/components/SessionsItem.vue
+++ src/components/SessionsItem.vue
@@ -1,24 +1,16 @@
 <template>
   <div class="p-4">
-    <div v-if="sessionStore.sessions?.length > 0" class="m-2 text-lg">Sessioni aperte: 
+    <div v-if="sessionStore.sessions?.length > 0" class="m-2 text-lg">Sessioni aperte:
 
       <div v-for="(session, index) in sessionStore.sessions" class="text-sm bg-gray-200 px-2 py-1 m-1 rounded">
-        <RouterLink
-            :to="`/session/${session.session_id}`"
-            class="text-blue-600 hover:underline text-sm"
-          >
-            {{ frontendBaseUrl }}/session/join/{{ session.session_id }} 
-          </RouterLink>
-          <RoleBadge :role="session.role" />
+        <RouterLink :to="`/session/${session.session_id}`" class="text-blue-600 hover:underline text-sm">
+          {{ frontendBaseUrl }}/session/join/{{ session.session_id }}
+        </RouterLink>
+        <RoleBadge :role="session.role" />
       </div>
     </div>
 
-    <ActionButton
-      @click="createSession"
-      :disabled="loading"
-      color="blue"
-      label="Crea una nuova sessione"
-    >
+    <ActionButton @click="createSession" :disabled="loading" color="blue" label="Crea una nuova sessione">
     </ActionButton>
   </div>
 </template>
@@ -28,6 +20,7 @@ import ActionButton from '@/components/buttons/ActionButton.vue'
 import { useSessionStore } from '@/stores/sessionStore'
 import { onMounted, ref } from 'vue'
 import RoleBadge from './RoleBadge.vue'
+import { supabase } from '@/lib/supabase'
 
 const loading = ref(false)
 const sessionStore = useSessionStore()
@@ -43,6 +36,21 @@ const createSession = async () => {
 
   const data = await res.json()
   sessionStore.getAllSessions()
+
+  const sessionId = data['session_id'];
+
+  const { error } = await supabase.from('session_state').insert([
+    {
+      session_id: sessionId,
+      match: {},         // o null, a seconda della struttura
+      events: [],        // array vuoto inizialmente
+    }
+  ])
+
+  if (error) {
+    console.error('Errore durante inizializzazione session_state:', error)
+  }
+
   loading.value = false
 }
 
