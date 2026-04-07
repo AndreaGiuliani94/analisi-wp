3fcc6db48bdc24b398611b2c1a1c6882f9a65a31
AndreaGiuliani94 <andreagiuliani.1994@gmail.com>
2026-04-07 17:36:55 +0200

    pnpm update

--- src/components/ShotMap.vue
+++ src/components/ShotMap.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="p-2">
 
-        <div class="relative w-full max-w-lg mx-auto aspect-[4/3] bg-cyan-50 border-2 border-cyan-200 rounded-lg">
+        <div class="relative w-full max-w-lg mx-auto aspect-4/3 bg-cyan-50 border-2 border-cyan-200 rounded-lg">
 
             <div class="absolute inset-0 overflow-hidden rounded-t-lg z-0">
 
-                <div class="absolute inset-0 opacity-20 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-cyan-300 via-cyan-100 to-transparent"></div>
+                <div class="absolute inset-0 opacity-20 bg-[radial-gradient(ellipse_at_center,var(--tw-gradient-stops))] from-cyan-300 via-cyan-100 to-transparent"></div>
     
                 <div class="absolute top-0 left-1/2 -translate-x-1/2 w-1/5 h-[8%] border-x-4 border-b-4 border-white bg-blue-900/10 z-10 shadow-inner"></div>
 
-                <div class="absolute top-[20%] w-full h-[2px] bg-red-500 shadow-[0_0_5px_rgba(239,68,68,0.5)] z-0"></div>
+                <div class="absolute top-[20%] w-full h-0.5 bg-red-500 shadow-[0_0_5px_rgba(239,68,68,0.5)] z-0"></div>
                 <span class="absolute top-[20%] left-2 -translate-y-1/2 text-[9px] font-bold text-red-600 bg-cyan-50 px-1">2m</span>
                 
                 <div v-if="viewMode=='even'">
-                    <div class="absolute top-[60%] w-full h-[2px] bg-yellow-400 shadow-[0_0_5px_rgba(250,204,21,0.5)] z-0"></div>
+                    <div class="absolute top-[60%] w-full h-0.5 bg-yellow-400 shadow-[0_0_5px_rgba(250,204,21,0.5)] z-0"></div>
                     <span class="absolute top-[60%] left-2 -translate-y-1/2 text-[9px] font-bold text-yellow-600 bg-cyan-50 px-1">6m</span>
                 </div>
                 <div v-else-if="viewMode=='sup'">
-                    <div class="absolute top-[50%] w-full h-[2px] bg-yellow-400 shadow-[0_0_5px_rgba(250,204,21,0.5)] z-0"></div>
+                    <div class="absolute top-[50%] w-full h-0.5 bg-yellow-400 shadow-[0_0_5px_rgba(250,204,21,0.5)] z-0"></div>
                     <span class="absolute top-[50%] left-2 -translate-y-1/2 text-[9px] font-bold text-yellow-600 bg-cyan-50 px-1">5m</span>
                 </div>
 
