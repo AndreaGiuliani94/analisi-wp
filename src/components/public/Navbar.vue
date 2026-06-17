788a34c118c30592ebd8bb45225f7c28fd9cc725
AndreaGiuliani94 <andreagiuliani.1994@gmail.com>
2026-06-17 12:48:22 +0200

    fix: update navbar link color for improved visibility

--- src/components/public/Navbar.vue
+++ src/components/public/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="flex justify-between items-center p-4  mx-auto w-full">
-    <router-link to="/" class=" flex justify-between items-center text-2xl font-black tracking-tighter text-blue-900">
+    <router-link to="/" class=" flex justify-between items-center text-2xl font-black tracking-tighter text-blue-950">
       <img src="/img/icons/logo.svg" class="w-10 h-10 inline-block mr-2" alt="Logo WP Studio" />
       <span>WP STUDIO</span>
     </router-link>
